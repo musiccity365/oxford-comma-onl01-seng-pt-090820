2654ef2bf3f03d0d5658d0d204065cbc8663bd98
Michael David Sackstein <m_sackstein@yahoo.com>
2020-09-15 23:02:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,11 @@ require 'pry'
 
 def oxford_comma(array)
   case array.length
-end 	when 1
-  "#{array[0]}"
-when 2
-  array[0..1].join(" and ")
-else
-  array[0...-1].join(", ") << ", and #{array[-1]}"
-end
+  when 1
+    "#{array[0]}"
+  when 2
+    array[0..1].join(" and ")
+  else
+    array[0...-1].join(", ") << ", and #{array[-1]}"
+  end
 end
